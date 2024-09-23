1f9d10c5f79bf73c4087662549a0d8d2e71f2928
Joy <joynisha.sumpter@gmail.com>
2024-09-23 15:02:29 -0400

    Initial Upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+with st.sidebar:
+    st.write("To chat with Unity Wiz ask them a question about Unity!")
+
+st.header("UNITY WIZ!")
+st.text_input("Ask me a question about Unity!")
