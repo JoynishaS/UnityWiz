4780b6565171cec3464e8cdf656ac1dc329b7f79
Joy <joynisha.sumpter@gmail.com>
2024-09-28 23:41:02 -0400

    Finally works without the ghost text

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ Settings.text_splitter = SentenceSplitter(chunk_size=500,chunk_overlap=20)
 Settings.embed_model = NVIDIAEmbedding(model = "NV-Embed-QA", truncate="END", api_key= st.secrets['NVIDIA_API_KEY'] )
 Settings.llm = NVIDIA(model = "meta/llama-3.2-3b-instruct")
 
-
 def loadUnityDocumentation():
+    st.set_page_config(layout="wide")
     st.session_state['initialized'] = True
 
     # Get Pre-existing Milvus vector store and storage context
@@ -38,81 +38,79 @@ def chat(message):
         st.session_state['response'] = st.session_state['query_engine'].query(message)
         stream_response(st.session_state['response'])
     except Exception as e:
-        st.error(f"Error proccessing query:{str(e)}")
+        st.error(f"Error processing query:{str(e)}")
 
-#Function to stream responses
+#Function to get chat responses
 def stream_response(message):
-    if st.session_state['query_engine'] is None:
-        st.write("UnityWiz is waiting for a question!")
-    try:
-        st.session_state['response'] = st.session_state['query_engine'].query(message)
+    with st.chat_message("assistant"):
+        st.session_state['results'] = st.empty()
         full_response = ""
+        if st.session_state['query_engine'] is None:
+            st.write("UnityWiz is waiting for a question!")
+        st.session_state['response'] = st.session_state['query_engine'].query(message)
         for text in st.session_state['response'].response_gen:
             full_response += text
-            st.session_state['full_response'] = full_response
-    except Exception as e:
-        st.error(f"Error proccessing query:{str(e)}")
+            st.session_state['results'].markdown(full_response)
+    st.session_state['history'].append({"role": "assistant", "content": full_response})
 
-#Load Unity Documentation
-if 'initialized' not in st.session_state:
-    loadUnityDocumentation()
 
-#interface
-with st.sidebar:
-    st.write("To chat with Unity Wiz ask them a question about Unity!")
+def main():
+    #Load Unity Documentation
+    if 'initialized' not in st.session_state:
+        loadUnityDocumentation()
 
-col1 = st.columns([1])
+    #Side Bar Interface
+    with st.sidebar:
+        st.write("To chat with Unity Wiz ask them a question about Unity!")
 
-st.title("UNITY WIZ!")
+    #Chat Bot Interface
+    st.title("UNITY WIZ!")
+    user_input = st.chat_input("Ask me a question about Unity!")
 
-user_input = st.chat_input("Ask me a question about Unity!")
+    #Display chat messages
+    chat_container = st.container()
+    with chat_container:
+        for message in st.session_state['history']:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
 
-#Display chat messages
-chat_container = st.container()
-with chat_container:
-    for message in st.session_state['history']:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+    if user_input:
+        with st.chat_message("user"):
+            st.markdown(user_input)
+        st.session_state['history'].append({"role":"user","content":user_input})
+        stream_response(user_input)
 
-if user_input:
-    with st.chat_message("user"):
-        st.markdown(user_input)
-    st.session_state['history'].append({"role":"user","content":user_input})
-    stream_response(user_input)
+    #Move Clear Button to Bottom of Screen
+    st.markdown(f"""
+      <style>
+      [class="st-emotion-cache-h23xut ef3psqc13"]{{
+        display: flex;
+        -webkit-box-align: center;
+        align-items: center;
+        -webkit-box-pack: center;
+        justify-content: center;
+        font-weight: 400;
+        padding: 0.25rem 0.75rem;
+        border-radius: 0.5rem;
+        min-height: 2.5rem;
+        margin-top: 5px;
+        line-height: 1.6;
+        color: inherit;
+        width: auto;
+        user-select: none;
+        background-color: rgb(29, 27, 27);
+        border: 1px solid rgba(241, 242, 245, 0.2);
+        position: fixed;
+        bottom:0;
+        z-index:9999;
+      }}
+      </style>
+    """, unsafe_allow_html=True)
 
-    with st.chat_message("assistant"):
-        results = st.markdown(st.session_state['full_response'])
-    st.session_state['history'].append({"role": "assistant", "content": st.session_state['full_response']})
+    if st.button("Clear"):
+        st.session_state['history'] = []
+        st.rerun()
 
-#Move Clear Button to Bottom of Screen
-st.markdown(f"""
-  <style>
-  [class="st-emotion-cache-h23xut ef3psqc13"]{{
-    display: flex;
-    -webkit-box-align: center;
-    align-items: center;
-    -webkit-box-pack: center;
-    justify-content: center;
-    font-weight: 400;
-    padding: 0.25rem 0.75rem;
-    border-radius: 0.5rem;
-    min-height: 2.5rem;
-    margin-top: 5px;
-    line-height: 1.6;
-    color: inherit;
-    width: auto;
-    user-select: none;
-    background-color: rgb(29, 27, 27);
-    border: 1px solid rgba(241, 242, 245, 0.2);
-    position: fixed;
-    bottom:0;
-    z-index:9999;
-  }}
-  </style>
-"""
-            , unsafe_allow_html=True)
 
-if st.button("Clear"):
-    st.session_state['history'] = []
-    st.rerun()
-
+if __name__ == "__main__":
+    main()
